17dc3e0f58e4ca8f7d533accb0b003099ae44206
alexandrp <alexandrp@saltedge.com>
2017-12-06 17:44:11 +0200

    give 'em some errors

--- ghost_methods/cellar.rb
+++ ghost_methods/cellar.rb
@@ -6,28 +6,34 @@ class Cellar
   end
 
   def deposit_potatoes(kilos)
+    raise "Should be Integer" unless kilos.is_a? Integer
     @potatoes += kilos
   end
 
   def take_potatoes(kilos)
+    raise "Should be Integer" unless kilos.is_a? Integer
     raise "Not enough potatoes" if kilos > @potatoes
     @potatoes -= kilos
   end
 
   def deposit_pickles(jars)
+    raise "Should be Integer" unless jars.is_a? Integer
     @pickles += jars
   end
 
   def take_pickles(jars)
+    raise "Should be Integer" unless jars.is_a? Integer
     raise "Not enough pickles" if jars > @pickles
     @pickles -= jars
   end
 
   def deposit_wine(bottles)
+    raise "Should be Integer" unless bottles.is_a? Integer
     @wine += bottles
   end
 
   def take_wine(bottles)
+    raise "Should be Integer" unless bottles.is_a? Integer
     raise "Not enough wine" if bottles > @wine
     @wine -= bottles
   end
--- ghost_methods/warehouse.rb
+++ ghost_methods/warehouse.rb
@@ -11,10 +11,12 @@ class Warehouse
 
   GOODS.each do |good|
     define_method "deposit_#{good}" do |quantity|
+      raise "Should be Integer" unless quantity.is_a? Integer
       self.instance_eval("@#{good} += #{quantity}")
     end
 
     define_method "take_#{good}" do |quantity|
+      raise "Should be Integer" unless quantity.is_a? Integer
       raise "Not enough #{good}" if quantity > self.public_send(good)
       self.instance_eval("@#{good} -= #{quantity}")
     end
